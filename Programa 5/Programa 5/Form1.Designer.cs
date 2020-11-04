eb874a3cf1fa3ad76620d0b5d4c50da37543f91c
Rui Rodrigues <73064724+RuiRodrigues-lab@users.noreply.github.com>
2020-11-04 01:44:37 +0000

    Add files via upload

---
+++ Programa 5/Programa 5/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Programa_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSaldoAtual = new System.Windows.Forms.TextBox();
+            this.txtSaldoPretendido = new System.Windows.Forms.TextBox();
+            this.txtTaxa = new System.Windows.Forms.TextBox();
+            this.txtAnosNecessarios = new System.Windows.Forms.TextBox();
+            this.txtSaldoFinal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSaldoAtual
+            // 
+            this.txtSaldoAtual.Location = new System.Drawing.Point(199, 25);
+            this.txtSaldoAtual.Name = "txtSaldoAtual";
+            this.txtSaldoAtual.Size = new System.Drawing.Size(84, 20);
+            this.txtSaldoAtual.TabIndex = 0;
+            // 
+            // txtSaldoPretendido
+            // 
+            this.txtSaldoPretendido.Location = new System.Drawing.Point(199, 51);
+            this.txtSaldoPretendido.Name = "txtSaldoPretendido";
+            this.txtSaldoPretendido.Size = new System.Drawing.Size(84, 20);
+            this.txtSaldoPretendido.TabIndex = 1;
+            // 
+            // txtTaxa
+            // 
+            this.txtTaxa.Location = new System.Drawing.Point(197, 77);
+            this.txtTaxa.Name = "txtTaxa";
+            this.txtTaxa.Size = new System.Drawing.Size(84, 20);
+            this.txtTaxa.TabIndex = 2;
+            // 
+            // txtAnosNecessarios
+            // 
+            this.txtAnosNecessarios.Location = new System.Drawing.Point(72, 176);
+            this.txtAnosNecessarios.Name = "txtAnosNecessarios";
+            this.txtAnosNecessarios.ReadOnly = true;
+            this.txtAnosNecessarios.Size = new System.Drawing.Size(66, 20);
+            this.txtAnosNecessarios.TabIndex = 3;
+            // 
+            // txtSaldoFinal
+            // 
+            this.txtSaldoFinal.Location = new System.Drawing.Point(138, 221);
+            this.txtSaldoFinal.Name = "txtSaldoFinal";
+            this.txtSaldoFinal.ReadOnly = true;
+            this.txtSaldoFinal.Size = new System.Drawing.Size(117, 20);
+            this.txtSaldoFinal.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 116);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(290, 31);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Saldo Actual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "$";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Saldo Final Pretendido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(122, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Taxa de Juro";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(287, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Ao fim de ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(144, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "anos";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 221);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(120, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Conseguira um saldo de";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(261, 224);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "dolares";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 266);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSaldoFinal);
+            this.Controls.Add(this.txtAnosNecessarios);
+            this.Controls.Add(this.txtTaxa);
+            this.Controls.Add(this.txtSaldoPretendido);
+            this.Controls.Add(this.txtSaldoAtual);
+            this.Name = "Form1";
+            this.Text = "Programa 5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSaldoAtual;
+        private System.Windows.Forms.TextBox txtSaldoPretendido;
+        private System.Windows.Forms.TextBox txtTaxa;
+        private System.Windows.Forms.TextBox txtAnosNecessarios;
+        private System.Windows.Forms.TextBox txtSaldoFinal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Programa 5/Programa 5/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double dblSaldoAtual = Convert.ToDouble(txtSaldoAtual.Text);
+            double dblSaldoPretendido = Convert.ToDouble(txtSaldoPretendido.Text);
+            double dblTaxa = Convert.ToDouble(txtTaxa.Text) / 100;
+            int nAnos = 0;
+            //a app ignora o facto de o saldo pretendido já ter sido alcançado pois o loop faz o "do" obrigatoriamente
+            while (dblSaldoAtual < dblSaldoPretendido)
+            {
+                dblSaldoAtual = dblSaldoAtual + dblSaldoAtual * dblTaxa;
+                nAnos = nAnos + 1;
+            }
+            /*ambas as formas são eficazes para corrigir o problema, porem alterar o operador para apenas < e apenas while
+             * resolve o problema de forma mais eficiente a nivel de recursos do computador*/
+            txtSaldoFinal.Text = dblSaldoAtual.ToString();
+            txtAnosNecessarios.Text = nAnos.ToString();
+        }
+    }
+}
